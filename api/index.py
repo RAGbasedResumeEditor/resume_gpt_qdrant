c58765def34b65c6b63de061d9970e2e5bac4fce
creatub <blatherr@gmail.com>
2024-05-23 16:54:25 +0900

    commit 34 json rewrite -> routes

--- api/index.py
+++ api/index.py
@@ -59,7 +59,7 @@ def process():
     )
 
     #OpenAI Model
-    llm = ChatOpenAI(model=model, temperature=temperature) # 1.5부터 Internal Error 발생
+    llm = ChatOpenAI(model=model, temperature=temperature) # 0~1.3
 
     # Retriever 3개 까지
     retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
