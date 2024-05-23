859ab29ae9ea2f4784bc12bbb93f67fe63d64c2a
creatub <blatherr@gmail.com>
2024-05-23 16:24:59 +0900

    commit 30 CORS Credentials 추가

--- api/index.py
+++ api/index.py
@@ -45,6 +45,8 @@ def process():
         api_key=os.environ["QDRANT_API_KEY"]
     )
     os.environ['OPENAI_API_KEY'] =os.environ["OPENAI_API_KEY"]
+    
+    #embedding
     embeddings = OpenAIEmbeddings()
 
 
